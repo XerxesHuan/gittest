8b316fbcdd1c3efa2e8bcdf704cfb989b6b3eea6
XerxesHuan <303996563@qq.com>
2020-04-14 17:35:59 +0800

    XerxesHuan做的一个User entity

---
+++ src/main/java/com/gittest/entity/User.java
@@ -0,0 +1,17 @@
+package com.gittest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer userId;
+    private String userName;
+    private String password;
+
+}
